30f285554a6327e89c12bbf1d68a1bc595fce7ae
Mikko <mikko@mfabrik.com>
2012-05-07 22:24:58 +0300

    test commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""
+
+    Declare a Python package silvuple
+
+    See 
+
+    * http://wiki.python.org/moin/Distutils/Tutorial
+
+    * http://packages.python.org/distribute/setuptools.html#developer-s-
+
+    * http://plone.org/products/plone/roadmap/247
+
+"""
+
+from setuptools import setup
+
+setup(name = "silvuple",
+    version = "0.0",
+    description = "Translation manager for Plone / LinguaPlone websites",
+    author = "Mikko Ohtamaa",
+    author_email = "mikko@opensourcehacker.com",
+    url = "http://opensourcehacker.com",
+    install_requires = ["five.grok", "z3c.jbot", "plone.app.dexterity", "plone.app.registry", "collective.js.jqueryui"],
+    packages = ['silvuple'],
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+    ],     
+    license="GPL2",
+    include_package_data = True,
+    entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,        
+) 
